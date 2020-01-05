32e132e3313a2e96988c22c78acd51c58d2f5b8f
Michal Bližňák <michal.bliznak@gmail.com>
2020-01-05 21:56:20 +0100

    Minor cleanup in HttpServer.

--- remoting-http/remoting-http-server/src/main/java/org/codedesigner/http/server/HttpServer.java
+++ remoting-http/remoting-http-server/src/main/java/org/codedesigner/http/server/HttpServer.java
@@ -3,17 +3,17 @@ package org.codedesigner.http.server;
 import org.codedesigner.remoting.api.CabBookingService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.remoting.httpinvoker.HttpInvokerServiceExporter;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class HttpServer {
 
-    @Bean(name = "/booking") HttpInvokerServiceExporter accountService() {
+    @Bean(name = "/booking")
+    HttpInvokerServiceExporter accountService() {
         HttpInvokerServiceExporter exporter = new HttpInvokerServiceExporter();
         exporter.setService( new CabBookingServiceImpl() );
         exporter.setServiceInterface( CabBookingService.class );
